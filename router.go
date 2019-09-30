b510f886b7f41194b90d3582e98250b520b01118
SHK <bedrook@gmail.com>
2019-10-01 04:51:00 +0900

    개발 모드 추가

--- router.go
+++ router.go
@@ -1,8 +1,9 @@
 package router
 
-import "net/http"
-import "strings"
-import "fmt"
+import (
+	"net/http"
+	"strings"
+)
 
 //Handler : http요청을 처리하기 위한 인터페이스
 type Handler interface {
@@ -12,18 +13,27 @@ type Handler interface {
 //Router : http 요청을 처리하는 핸드러를 담고있는 map
 //[method][path] 구조이다.
 type Router struct {
-	Handlers map[string]map[string]http.HandlerFunc
+	Handlers map[string]map[string]HandlerFunc
+	DevMode  bool
 }
 
 //HandleFunc : Router에 http 핸들러를 추가한다.
-func (router *Router) HandleFunc(method, pattern string, h http.HandlerFunc) {
+func (router *Router) HandleFunc(method, pattern string, h HandlerFunc) {
 	//http 메서드로 등록된 맵이 있는지 확인
 	m, ok := router.Handlers[method]
 	if !ok {
 		//등록된 맵이 없다면 새로운 맵을 생성
-		m = make(map[string]http.HandlerFunc)
+		m = make(map[string]HandlerFunc)
+
 		router.Handlers[method] = m
 	}
+
+	//개발모드라면 디버깅에 용이한 정보를 출력하도록 핸들러를 감싼다
+
+	if router.DevMode {
+		h = RecoverHandler(LogHandler(h))
+	}
+
 	//http 메서드로 등록된 맵에 URL 패턴과 핸들러 함수 등록
 	m[pattern] = h
 }
@@ -36,8 +46,7 @@ func match(pattern, path string) (bool, map[string]string) {
 	paths := strings.Split(path, "/")
 
 	if len(patterns) != len(paths) {
-		// fmt.Println("1", len(patterns), len(paths))
-		// fmt.Println("1", patterns, paths)
+
 		return false, nil
 	}
 
@@ -47,10 +56,10 @@ func match(pattern, path string) (bool, map[string]string) {
 		switch {
 		case patterns[i] == paths[i]:
 		case len(patterns[i]) > 0 && patterns[i][0] == ':':
-			//fmt.Println("param!", patterns[i], paths[i])
+
 			params[patterns[i][1:]] = paths[i]
 		default:
-			// fmt.Println("2", patterns, paths)
+
 			return false, nil
 		}
 	}
@@ -61,8 +70,25 @@ func (router *Router) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	for pattern, handler := range router.Handlers[req.Method] {
 		if ok, params := match(pattern, req.URL.Path); ok {
 			//요청 URL 에 해당하는 핸들러 수행
-			fmt.Println(pattern, params)
-			handler(res, req)
+			// debug code
+			// fmt.Println(pattern, params)
+
+			c := Context{
+				Param:          make(map[string]interface{}),
+				ResponseWriter: res,
+				Request:        req,
+			}
+
+			for k, v := range params {
+				c.Param[k] = v
+			}
+
+			if router.DevMode {
+				//개발모드 일 땐 Access-Control-Allow-Origin 을 모두 허용
+				c.ResponseWriter.Header().Set("Access-Control-Allow-Origin", "*")
+			}
+
+			handler(&c)
 			return
 		}
 	}
